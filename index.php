12fb80c774d2d5694305e6bd1569d1b452c63ae2
Rick <rick@iugo.io>
2016-05-09 10:30:37 -0700

    first commit

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+    if(isset($_SERVER["fav_food_str"])){
+        //do nothing we are in production and have ENV vars already
+    }else{
+        include "localhost_conf.php";
+    }
+    define("FAV_FOOD", $_SERVER["fav_food_str"]);
+    define("FAV_MUSIC", $_SERVER["fav_music_str"]);
+    print "My fav food is ".FAV_FOOD." and my fav music is ".FAV_MUSIC;
+?>
---
+++ localhost_conf.php
@@ -0,0 +1,4 @@
+<?php
+    $_SERVER["fav_food_str"] = "pizza";
+    $_SERVER["fav_music_str"] = "rock";
+?>
